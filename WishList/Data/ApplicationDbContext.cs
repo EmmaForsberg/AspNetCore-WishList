dc017f32908a8a0cba332a57f5abc20212b1d150
Emma Forsberg <32894170+EmmaForsberg@users.noreply.github.com>
2021-10-29 14:38:35 +0200

    created class and contructor

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options)
+        :base(options)
+    {
+
+    }
+
+
+
+}
