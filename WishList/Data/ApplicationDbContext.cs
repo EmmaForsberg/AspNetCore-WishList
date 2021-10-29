03b3e5cedb2d6946acafb71a08503ac4c2eba744
Emma Forsberg <32894170+EmmaForsberg@users.noreply.github.com>
2021-10-29 15:19:05 +0200

    Added dbset

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
 namespace WishList.Data
 { 
 
  public class ApplicationDbContext : DbContext
  {
-    public ApplicationDbContext(DbContextOptions options)
+        public DbSet<Item> Items { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options)
         :base(options)
     {
 
